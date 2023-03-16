a533fdd4201aab5cc2d9acf2b09bb6a3bd2a950c
sraeuchle <sraeuchle@3pc.de>
2023-03-16 13:30:43 +0100

    fix(property-expansion): handle project version properly

--- src/main/kotlin/dreipc/plugins/development/modul/GradlePropertyExpansion.kt
+++ src/main/kotlin/dreipc/plugins/development/modul/GradlePropertyExpansion.kt
@@ -19,59 +19,22 @@ import org.gradle.language.jvm.tasks.ProcessResources
  */
 class GradlePropertyExpansion : Plugin<Project> {
 
-    inline fun <reified C> Project.configure(name: String, configuration: C.() -> Unit) {
-        (this.tasks.getByName(name) as C).configuration()
-    }
-
     @Suppress("UnstableApiUsage") // ignore @Incubating
     override fun apply(project: Project) {
-//        val processResources = project.tasks.getByName("processResources") as ProcessResources
+        val processResources = project.tasks.getByName("processResources") as ProcessResources
 
-        project.configure<ProcessResources>("processResources") {
-            from("src/main/resources")
-            include("**/application*.yml", "**/application*.properties")
+        with(processResources) {
+            project.logger.info("Expand Gradle Properties")
             duplicatesStrategy = DuplicatesStrategy.INCLUDE
-            project.properties.forEach { prop ->
-                if (prop.value != null) {
-                    filter<ReplaceTokens>("tokens" to mapOf(prop.key.toString() to prop.value.toString()))
-                    filter<ReplaceTokens>("tokens" to mapOf("project." + prop.key.toString() to prop.value.toString()))
+            from("src/main/resources") {
+                include(".*", "**/application*.yml", "**/application*.properties")
+                project.properties.forEach { prop ->
+                    if (prop.value != null) {
+                        filter<ReplaceTokens>("tokens" to mapOf(prop.key.toString() to prop.value.toString()))
+                        filter<ReplaceTokens>("tokens" to mapOf("project." + prop.key.toString() to prop.value.toString()))
+                    }
                 }
             }
         }
     }
-//            tasks.withType<ProcessResources> {
-//                from
-//            }
-//        }
-//            tasks{
-//                processResources{
-//
-//                }
-//            }
-//            task<processResources{
-//
-//            }
-//        }
-//        with(processResources) {
-//            project.logger.info("Expand Gradle Properties")
-//
-//            from("src/main/resources")
-//            include("**/application*.yml", "**/application*.properties")
-//            duplicatesStrategy = DuplicatesStrategy.INCLUDE
-//            project.properties.forEach { prop ->
-//                if (prop.value != null) {
-//                    filter<ReplaceTokens>("tokens" to mapOf(prop.key.toString() to prop.value.toString()))
-//                    filter<ReplaceTokens>("tokens" to mapOf("project." + prop.key.toString() to prop.value.toString()))
-//                }
-//            }
-//            doLast{
-//                val semver = project.extensions["semver"] as SemverGitPluginExtension
-//                val version = System.getenv("CI_APP_VERSION") ?: semver.info.toString()
-//                System.out.println("Version now: ${version}")
-//
-//                filter<ReplaceTokens>("tokens" to mapOf("project." + prop.key.toString() to prop.value.toString()))
-//
-//            }
-//        }
-//    }
 }
