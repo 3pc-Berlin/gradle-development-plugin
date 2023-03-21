794822582e8ce9acb96b2e39f835d32434361b68
sraeuchle <sraeuchle@3pc.de>
2023-03-21 14:50:07 +0100

    feat(docker): add .dockerignore file + some refactoring

--- src/main/kotlin/dreipc/plugins/development/modul/Docker.kt
+++ src/main/kotlin/dreipc/plugins/development/modul/Docker.kt
@@ -3,25 +3,25 @@ package dreipc.plugins.development.modul
 import com.bmuschko.gradle.docker.DockerJavaApplicationPlugin
 import com.bmuschko.gradle.docker.tasks.AbstractDockerRemoteApiTask
 import com.bmuschko.gradle.docker.tasks.RegistryCredentialsAware
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.bmuschko.gradle.docker.tasks.image.DockerPushImage
+import com.bmuschko.gradle.docker.tasks.image.DockerRemoveImage
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.internal.os.OperatingSystem.*
-import org.gradle.kotlin.dsl.get
+import org.gradle.internal.os.OperatingSystem.current
+import java.io.File
 
 
 class Docker : Plugin<Project> {
 
     private val DEFAULT_REPO_URL = "nexus.3pc.de"
-    private val DOCKER_BASE_IMAGES = mapOf(17 to "nexus.3pc.de/java:17")
-    private var DEFAULT_EXPOSE_PORT: Int = 8080
-    private var javaOptsDefault: String = """
-         -Djava.security.egd=file:/dev/./urandom -XX:TieredStopAtLevel=1
-         -Dspring.jmx.enabled=true -Dspring.config.location=classpath:/application.yml
-         -Djava.awt.headless=true
-         -Dfile.encoding=UTF-8 -DLC_CTYPE=UTF-8
-    """.trimIndent()
+    private val DOCKER_BASE_IMAGE = "nexus.3pc.de/java"
+    private var DEFAULT_EXPOSE_PORT = 8080
+    private var JAVA_OPTS_DEFAULT = " -Djava.security.egd=file:/dev/./urandom -XX:TieredStopAtLevel=1" +
+            " -Dspring.jmx.enabled=true -Dspring.config.location=classpath:/application.yml" +
+            " -Djava.awt.headless=true" +
+            " -Dfile.encoding=UTF-8 -DLC_CTYPE=UTF-8"
 
     override fun apply(project: Project) {
         project.plugins.apply(DockerJavaApplicationPlugin::class.java)
@@ -30,6 +30,16 @@ class Docker : Plugin<Project> {
         configureDockerAPI(project)
 
         createDockerFileTask(project)
+        buildImageTask(project)
+        pushImageTask(project)
+        removeImageTask(project)
+
+        addDockerIgnore()
+    }
+
+    private fun buildImageName(project: Project): String {
+        return System.getenv("CI_DOCKER_NAMEONLY")
+            ?: "${DEFAULT_REPO_URL}/${project.name}:${project.version}" // allow CI to override image path (e.g. add /temp/ to the path)
     }
 
     private fun configureRegistryCredentials(
@@ -42,10 +52,10 @@ class Docker : Plugin<Project> {
 
         project.tasks.withType(RegistryCredentialsAware::class.java)
             .configureEach {
-                this.registryCredentials.url.set(repoUrl)
-                this.registryCredentials.username.set(user)
-                this.registryCredentials.password.set(password)
-                this.registryCredentials.email.set(email)
+                registryCredentials.url.set(repoUrl)
+                registryCredentials.username.set(user)
+                registryCredentials.password.set(password)
+                registryCredentials.email.set(email)
             }
     }
 
@@ -55,28 +65,66 @@ class Docker : Plugin<Project> {
         val daemonUrl = if (current().isWindows()) "tcp://127.0.0.1:2375" else "unix:///var/run/docker.sock"
 
         project.tasks.withType(AbstractDockerRemoteApiTask::class.java)
-            .configureEach {
-                this.url.set(daemonUrl)
-            }
+            .configureEach { this.url.set(daemonUrl) }
     }
 
-    private fun createDockerFileTask(project: Project){
-        project.tasks.register("createDockerfile", Dockerfile::class.java){
+    private fun createDockerFileTask(project: Project) = project.afterEvaluate {
+        project.tasks.register("createDockerfile", Dockerfile::class.java) {
             group = "docker"
             destFile.set(project.file("${project.buildDir}/Dockerfile"))
 
-            val javaPlugin = project.extensions.get("java") as JavaPluginExtension
-            val javaVersion = javaPlugin.sourceCompatibility.ordinal
+            val javaVersion = properties["sourceCompatibility"]
+            from("${DOCKER_BASE_IMAGE}:${javaVersion}")
 
-            from(DOCKER_BASE_IMAGES[javaVersion])
             exposePort(DEFAULT_EXPOSE_PORT)
-            environmentVariable("JAVA_OPTS_LOCAL_DEFAULTS",javaOptsDefault)
-            environmentVariable("JAVA_OPTS_HARDCODED","")
+            environmentVariable("JAVA_OPTS_LOCAL_DEFAULTS", JAVA_OPTS_DEFAULT)
+            environmentVariable("JAVA_OPTS_HARDCODED", "")
 
             val jarName = "${project.rootProject.name}-${project.rootProject.version}.jar"
             copyFile("libs/${jarName}", "/app.jar")
 
-            entryPoint("sh", "-c", "java \${JAVA_OPTS_GLOBAL_DEFAULTS_11:-\$JAVA_OPTS_LOCAL_DEFAULTS} \$JAVA_OPTS_SPECIFIC \$JAVA_OPTS_HARDCODED -jar /app.jar")
+            entryPoint(
+                "sh",
+                "-c",
+                "java \${JAVA_OPTS_GLOBAL_DEFAULTS_11:-\$JAVA_OPTS_LOCAL_DEFAULTS} \$JAVA_OPTS_SPECIFIC \$JAVA_OPTS_HARDCODED -jar /app.jar"
+            )
         }
+    }
+
+    private fun buildImageTask(project: Project) {
+        project.tasks.register("buildImage", DockerBuildImage::class.java) {
+            dependsOn("createDockerfile")
+            group = "docker"
+
+            inputDir.set(project.buildDir)
+            images.add(buildImageName(project))
+        }
+    }
+
+    private fun pushImageTask(project: Project) {
+        project.tasks.register("pushImage", DockerPushImage::class.java) {
+            group = "docker"
+            images.add(buildImageName(project))
+        }
+    }
+
+    private fun removeImageTask(project: Project) {
+        project.tasks.register("removeImage", DockerRemoveImage::class.java) {
+            group = "docker"
+            targetImageId(buildImageName(project))
+            force.set(true)
+            onError { System.out.println("No previous versioned image to delete") }
+        }
+    }
+
+    private fun addDockerIgnore() {
+        val dockerIgnoreFile = File(".dockerignore")
+
+        val dockerIgnoreContent = this::class.java.classLoader.getResourceAsStream(".dockerignore")
+            ?.bufferedReader()
+            ?.readText()
+            ?: ""
+
+        dockerIgnoreFile.writeText(dockerIgnoreContent, Charsets.UTF_8)
     }
 }
