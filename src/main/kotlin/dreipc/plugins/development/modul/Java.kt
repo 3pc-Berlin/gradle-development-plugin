8a23c0077c065a4a5039a6062ccb5858e8cc48de
sraeuchle <sraeuchle@3pc.de>
2023-03-21 14:51:17 +0100

    refactor: include java version into code settings

---
+++ src/main/kotlin/dreipc/plugins/development/modul/Java.kt
@@ -0,0 +1,22 @@
+package dreipc.plugins.development.modul
+
+import com.bmuschko.gradle.docker.shaded.org.apache.commons.lang3.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.get
+
+class Java : Plugin<Project> {
+    override fun apply(project: Project) {
+        if (!project.plugins.hasPlugin("java")) {
+            project.plugins.apply("java")
+        }
+
+        project.plugins.withType(JavaPlugin::class.java) {
+            val javaPluginExtension = project.extensions.get("java") as JavaPluginExtension
+            javaPluginExtension.setSourceCompatibility(JavaVersion.JAVA_17)
+            javaPluginExtension.setSourceCompatibility(JavaVersion.JAVA_17)
+        }
+    }
+}
