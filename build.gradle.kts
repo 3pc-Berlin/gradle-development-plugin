bc29b50ba2791917053bb792c4ef811e05c8f645
sraeuchle <sraeuchle@3pc.de>
2023-05-16 15:55:41 +0200

    build: using own plugin with new id

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	`kotlin-dsl`
 	kotlin("jvm") version "1.8.0"
 	id("com.gradle.plugin-publish") version "1.2.0"
-	id("dreipc.development") version "0.0.3"
+	id("de.3pc.development") version "0.0.4"
 }
 
 group = "de.dreipc"
