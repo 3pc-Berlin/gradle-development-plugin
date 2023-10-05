2e5e9bb6d2a96dc3326773eb5faa88b57f00a711
sraeuchle <sraeuchle@3pc.de>
2023-10-05 11:52:51 +0200

    build(deps): bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@
 plugins {
 	`java-gradle-plugin`
 	`kotlin-dsl`
-	kotlin("jvm") version "1.8.0"
+	kotlin("jvm") version "1.9.10"
 	id("com.gradle.plugin-publish") version "1.2.1"
-	id("de.3pc.development") version "0.0.10"
+	id("de.3pc.development") version "0.0.11"
 }
 
 group = "de.3pc"
@@ -35,21 +35,21 @@ dependencies {
 	implementation(kotlin("stdlib", "1.8.0"))
 
 	//Plugins see: https://plugins.gradle.org/
-	implementation("io.freefair.lombok:io.freefair.lombok.gradle.plugin:8.2.2")
+	implementation("io.freefair.lombok:io.freefair.lombok.gradle.plugin:8.3")
 	implementation("io.wusa.semver-git-plugin:io.wusa.semver-git-plugin.gradle.plugin:2.3.7")
 
-	implementation("com.diffplug.spotless:spotless-plugin-gradle:6.20.0")
+	implementation("com.diffplug.spotless:spotless-plugin-gradle:6.22.0")
 	implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.1.0")
 	implementation("net.ltgt.gradle:gradle-nullaway-plugin:1.6.0")
 
-	implementation("com.bmuschko:gradle-docker-plugin:9.3.2")
+	implementation("com.bmuschko:gradle-docker-plugin:9.3.4")
 
 	// Testing
 	testImplementation("org.assertj:assertj-core:3.24.2")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
 
-	testImplementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.2")
+	testImplementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
 }
 
 extensions.findByName("buildScan")?.withGroovyBuilder {
@@ -62,4 +62,3 @@ publishing {
 		mavenLocal()
 	}
 }
-
